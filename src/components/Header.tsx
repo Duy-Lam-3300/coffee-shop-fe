142e338e3767f7e0bb56b4d8cd9c6ad7cbc466ef
Duy Lâm Nguyễn <Duylam3300@gmail.com>
2025-05-07 14:49:04 +0700

    add reponsive to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,34 +1,47 @@
+"use client"
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 
 
 export default function Header() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const router = useRouter();
+
+    const NAV_ITEMS = [
+        { name: "Product", path: "/product" },
+        { name: "Special Offers", path: "/offers" },
+        { name: "The Process", path: "/process" },
+        { name: "Packing", path: "/packing" },
+        { name: "About", path: "/about" },
+    ];
+
+    const SECONDARY_ITEMS = [
+        { name: "Chat", path: "/chat" },
+        { name: "Cart", path: "/cart" },
+    ];
+
+    const handleRouting = (location: string) => {
+        router.push(location);
+        setIsMenuOpen(false);
+    }
+
     return (
-        <header className="p-10 font-semibold text-lg h-[10vh]">
-            <div className="flex justify-between max-w-7xl mx-auto items-center">
+        <header className="p-6 md:p-10 font-semibold text-lg">
+            <div className=" justify-between max-w-7xl mx-auto items-center flex ">
                 <div className="">
                     <Link href="/"><p >Coffee Shop</p></Link>
                 </div>
-                <nav>
-                    <ul className="flex space-x-6">
-                        <li>
-                            <Link href="/product" className="navItem">Product</Link>
-                        </li>
-                        <li>
-                            <Link href="" className="navItem">Special Offers</Link>
-                        </li>
-                        <li>
-                            <Link href="" className="navItem">The process</Link>
-                        </li>
-                        <li>
-                            <Link href="" className="navItem">Packing</Link>
-                        </li>
-                        <li>
-                            <Link href="" className="navItem">About</Link>
-                        </li>
+                {/* desktop */}
+                <nav className="md:flex hidden ">
+                    <ul className=" space-x-6 flex">
+                        {NAV_ITEMS.map((item) => (<li key={item.path}>
+                            <Link href={item.path} className="navItem text-black">{item.name}</Link>
+                        </li>))}
                     </ul>
                 </nav>
-                <div className="flex justify-between items-center space-x-6">
+                <div className="md:flex hidden justify-between items-center space-x-6">
                     <nav >
                         <ul className="flex justify-between space-x-4">
                             <li>Chat</li>
@@ -37,7 +50,32 @@ export default function Header() {
                     </nav>
                     <button>Login/Signup</button>
                 </div>
+                <div className="md:hidden">
+                    <button onClick={() => setIsMenuOpen(!isMenuOpen)} className="text-xl">
+                        ☰
+                    </button>
+                </div>
             </div>
+            {/* mobile */}
+            {isMenuOpen && (<div className="fixed md:hidden bg-black/70 h-full z-50 w-full inset-0 text-white backdrop-blur-sm transition-opacity duration-300">
+                <div className="relative w-[100vw] h-[100vh] p-10 ">
+                    <button onClick={() => setIsMenuOpen(false)} className="text-2xl absolute right-6 top-6" aria-label="Close menu">
+                        x
+                    </button>
+                    <nav className="mt-10">
+                        <ul className="space-y-4">
+                            {NAV_ITEMS.map((item) => (
+                                <li onClick={() => handleRouting(item.path)} key={item.path} className="border-b-1 border-white/20 py-4 flex justify-between pr-2"><span>{item.name}</span><span>→</span></li>
+                            ))}
+
+                            <li onClick={() => handleRouting("")} className="border-b-1 border-whit py-4 flex justify-between pr-2"><p>Chat</p><p>{">"}</p></li>
+                            <li onClick={() => handleRouting("")} className="border-b-1 border-whit py-4 flex justify-between pr-2"><p>Cart</p><p>{">"}</p></li>
+                            <li onClick={() => handleRouting("")} className="border-b-1 border-whit py-4  pr-2"><p>Login / Signup</p></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>)}
+
 
         </header>
     )
