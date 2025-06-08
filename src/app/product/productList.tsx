e72f06dd73b6c1980de7896aae402fa477c733fe
Duy Lâm Nguyễn <Duylam3300@gmail.com>
2025-06-09 01:08:29 +0700

    deployment #6

--- src/app/product/productList.tsx
+++ src/app/product/productList.tsx
@@ -91,6 +91,7 @@ export default function ProductList() {
         <div className="py-12 bg-white">
             <div className="max-w-7xl mx-auto ">
                 <div className="">
+                    <h2>s</h2>
                     {/* <h2 className={`font-bold text-5xl ${dancingScript.className}    `}>Products</h2> */}
                 </div>
                 <div className="flex gap-8">
