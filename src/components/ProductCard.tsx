3dae872820122065c02fdde24e3527758464a382
Duy Lâm Nguyễn <Duylam3300@gmail.com>
2025-06-11 22:20:16 +0700

    fix deployment #2

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { Coffee } from "lucide-react";
 import { Flame, Heart } from "lucide-react";
-import { cartServices } from "@/services/cartServices";
+import { cartServices } from "@/hooks/services/useCartServices";
 
 
 interface ProductCardProps {
